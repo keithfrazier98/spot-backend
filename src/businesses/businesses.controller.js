a1f7ca413d757bd83a06d7a25694ededf5fedb43
Keith Frazier <keithers98@gmail.com>
2021-09-23 21:34:17 -0700

    added validation for phone numbers, added error messages for invalid phone number and no results

--- src/businesses/businesses.controller.js
+++ src/businesses/businesses.controller.js
@@ -13,34 +13,43 @@ async function read(req, res, next) {
   let response;
   let firstResult;
   let prettyJson;
+  try {
+    if (req.query.phone) {
+      phone = req.query.phone;
+      phone = phone.split("");
 
-  console.log(req.query);
-  if (req.query.phone) {
-    phone = req.query.phone;
-    phone = phone.split("")
-    if(phone[0] != "+" && !phone[1] != "1"){
-        console.log("disecting")
-        phone.splice(0,0,"+1")
-        phone = phone.join("")
+      if (phone.length === 11 && phone[0] === "1") {
+        //one is present but + isnt
+        phone.splice(0, 0, "+");
+        phone = phone.join("");
+      } else if (phone.length === 10) {
+        // +1 isnt present
+        phone.splice(0, 0, "+1");
+        phone = phone.join("");
+      } else if (phone.length < 10 || phone.length > 11) {
+        return next({
+          status: 400,
+          message: "Check your phone number, it seems to be invalid!",
+        });
+      }
+      response = await client.phoneSearch({ phone: phone });
+    } else {
+      business = req.query.business;
+      location = req.query.location;
+      searchRequest = {
+        term: business,
+        location: location,
+      };
+      response = await client.search(searchRequest);
     }
-    console.log(phone)
-    response = await client.phoneSearch({ phone: phone });
-  } else {
-    business = req.query.business;
-    location = req.query.location;
-    searchRequest = {
-      term: business,
-      location: location,
-    };
-    response = await client.search(searchRequest);
+    firstResult = await response.jsonBody.businesses[0];
+    prettyJson = await JSON.stringify(firstResult, null, 4);
+    //Reapond with valid response
+    res.status(200).json({ data: prettyJson });
+  } catch (error) {
+    //call error handler
+    next({ status: 400, message: "There are no results for your search!" });
   }
-
-  firstResult = await response.jsonBody.businesses[0];
-  prettyJson = await JSON.stringify(firstResult, null, 4);
-
-  console.log(prettyJson);
-
-  res.status(200).json({ data: prettyJson });
 }
 
 module.exports = { read };
