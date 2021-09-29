f32b297af7070f49681784dfe0be503004f0dcd3
Keith Frazier <keithers98@gmail.com>
2021-09-29 11:55:35 -0700

    added category json file, returned json on category request

--- src/categories/categories.contoller.js
+++ src/categories/categories.contoller.js
@@ -1,14 +1,9 @@
 const yelp = require("yelp-fusion");
 const client = yelp.client(process.env.API_KEY);
+const simpleCats = require('./simpleCats.json')
 
-async function getAllCategories(req, res, next) {
-  try {
-    const response = await client.allCategories();
-    const body = response.jsonBody.categories;
-    res.status(200).json({ data: body });
-  } catch (error) {
-    next({ status: 400, message: "Error fetching categories" });
-  }
+function getAllCategories(req, res, next) {
+    res.status(200).json({data:simpleCats.categories})
 }
 
 module.exports = { getAllCategories };
